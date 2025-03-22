b384116756247451ebd8f86b3efd8a1242b08d4a
Oxy45 <anondo.sreyan@gmail.com>
2025-03-22 22:29:52 +0600

    Third Commit

--- site.js
+++ site.js
@@ -1,1 +1,13 @@
 // <!-- Name: Anondo Sreyan, ID: 2220504  -->
+// <!-- Name: Anondo Sreyan, ID: 2220504  -->
+
+function showAlert() {
+    alert("Welcome to La Vie En Rose Store!");
+}
+function buyNow() {
+    alert("Thank you for your purchase!");
+}
+function submitForm(event) {
+    event.preventDefault();
+    alert("Your message has been sent!");
+}
